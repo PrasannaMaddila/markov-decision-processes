5d533c3fd639054989c743f41bafde01a8343887
Prasanna Maddila <maddilaprasanna10@gmail.com>
2023-05-04 11:14:45 +0200

    src/mdp.py: added strategy, P, reward too low

--- src/lunar_lander.py
+++ src/lunar_lander.py
@@ -5,7 +5,6 @@ observation, info = env.reset(seed=42)
 for _ in range(1000):
     action = env.action_space.sample()  # this is where you would insert your policy
     observation, reward, terminated, truncated, info = env.step(action)
-
-       if terminated or truncated:
-          observation, info = env.reset()
+    if terminated or truncated:
+        observation, info = env.reset()
 env.close()
